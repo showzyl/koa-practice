a75beba45d5799f1cc95c35372fe112ccac02be3
showzyl <showzyl520@gmail.com>
2017-08-09 14:58:25 +0800

    first ci

---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict'
+
+// demos/01.js
+const Koa = require('koa');
+const app = new Koa();
+const fs = require('fs');
+
+// const main = ctx => {
+//   if (ctx.request.accepts('xml')) {
+//     ctx.response.type = 'xml';
+//     ctx.response.body = '<data>Hello World</data>';
+//   } else if (ctx.request.accepts('json')) {
+//     ctx.response.type = 'json';
+//     ctx.response.body = { data: 'Hello World' };
+//   } else if (ctx.request.accepts('html')) {
+//     ctx.response.type = 'html';
+//     ctx.response.body = '<p>Hello World</p>';
+//   } else {
+//     ctx.response.type = 'text';
+//     ctx.response.body = 'Hello World';
+//   }
+// };
+
+// const main = ctx => {
+//   ctx.response.type = 'html';
+//   ctx.response.body = fs.createReadStream('./lizi.html');
+// };
+
+
+// route
+// const main = ctx => {
+//   if (ctx.request.path !== '/') {
+//     ctx.response.type = 'html';
+//     ctx.response.body = '<h3>我不是主页</h3><br/><a href="/">Index Page</a>';
+//   } else {
+//     ctx.response.body = '主页';
+//   }
+// };
+
+// const route = require('koa-route');
+
+// const about = ctx => {
+//   ctx.response.type = 'html';
+//   ctx.response.body = '<h3>我不是主页</h3><br/><a href="/">Index Page</a>';
+// };
+
+// const main = ctx => {
+//   ctx.response.body = 'Hello World';
+// };
+
+// app.use(route.get('/', main));
+// app.use(route.get('/about', about));
+
+// ctx.response.redirect('/');
+
+
+const path = require('path')
+const staticFile = require('koa-static');
+
+const main = staticFile(path.join(__dirname));
+
+
+app.use(main);
+
+app.listen(3000, () => {
+  console.log('3000 start...')
+})
+
+
